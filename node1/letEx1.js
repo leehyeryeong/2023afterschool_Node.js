71fe8255b9468c15a9be76eb096a448fedb7f9ad
leehyeryeong <w2108@e-mirim.hs.kr>
2023-03-14 20:02:13 +0900

    let과 var 차이 알아보기

---
+++ node1/letEx1.js
@@ -0,0 +1,16 @@
+//let, var의 차이점 알기
+let fruit = 'apple';
+console.log('fruit: ', fruit);
+
+fruit = 'grape';
+console.log('fruit: ', fruit);
+
+//let fruit = 'hello';
+
+var variable1 = 'hello';
+var variable1 = 'bye';
+
+console.log(variable1);
+
+//let의 경우 같은 이름을 가진 변수를 여러 개 만들 수 없지만, var는 가능하다.
+//var의 경우 같은 이름을 가진 변수 여러 개가 가능하며 나중에 적은 값을 출력해준다.
