757151b7b20b054ec2e5e8ec7f68d15b4c743073
leehyeryeong <w2108@e-mirim.hs.kr>
2023-03-28 19:48:13 +0900

    클라이언트 연결 이벤트 처리

--- node5/client_event.js
+++ node5/client_event.js
@@ -14,4 +14,15 @@ server.listen(port, function() {
 //이벤트는 on으로 처리 connection이 발생하면 내부적으로 socket 생성
 server.on('connection', function(socket) {
   console.log('클라이언트가 접속했습니다.: %s, %d', socket.remoteAddress, socket.remotePort);
+});
+
+//클라이언트의 요청(request) 이벤트 처리
+server.on('request', function(req, res) {
+  console.log('클라이언트 요청이 들어왔습니다.');
+  console.dir(req);
 })
+
+//서버 종료 이벤트 처리
+server.on('close', function() {
+  console.log('서버가 종료됩니다.');
+});
