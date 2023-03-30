acf290ded3e5b94d512c905f66787ec43531e157
leehyeryeong <w2108@e-mirim.hs.kr>
2023-03-30 19:38:24 +0900

    웹 서버 생성, 실행

---
+++ node6/http_module.js
@@ -0,0 +1,10 @@
+//웹 서버를 생성하고 실행합니다.
+require('http').createServer(function (request, response) {
+  //응답합니다.
+  response.writeHead(200, {'Content-Type': 'text/html'});
+  response.end('<h1>Hello Web Server with Node.js</h1>');
+}).listen(52273, function() {
+  console.log('Server Running at http://127.0.0.1:52273');
+});
+
+//실행 후, 브라우저에서 http://127.0.0.1:52273에 접속해 결과 확인
